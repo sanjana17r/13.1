0bd915a799acccb37a3ee53a070545126082e391
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2018-06-15 21:09:20 +0530

    Update 13.1.js

--- 13.1.js
+++ 13.1.js
@@ -1,20 +1,23 @@
 var str = "hello";
-var uppercase = str.toUpperCase()
-n=uppercase.length;
+var uppercase = str.toUpperCase()//the following string is convertd to uppercase
+n=uppercase.length;//this is the length of the string.
 
-let promiseConvertUppercase = new Promise(function(resolve,reject){
+let promiseConvertUppercase = new Promise(function(resolve,reject){//this is a promise
 
-    setTimeout(function(){
-        if (uppercase){
-       resolve(uppercase)
+
+    setTimeout(function(){//time is set to no.of seconds equal to string length
+        if (uppercase)
+        {
+           resolve(uppercase)//if the condition is true ,the promise gets resolved.
         }
-        else{
+        else
+        {//if the string is not converted to uppercase,it gets rejected,hence displaying the error message.
               reject("error 2");
         }
     },1000*n)
 });
-promiseConvertUppercase.then(function(result){
-console.log(result);
+promiseConvertUppercase.then(function(result){// promise is called
+      console.log(result);//prints HELLO.
 }).catch(function(result){
     console.log(result);
 });
